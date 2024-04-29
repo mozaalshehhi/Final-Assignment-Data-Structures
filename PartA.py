5cd6f0da036971856d6d821d165c8e0a5a0af5b8
ZainabMohsinn <160010747+ZainabMohsinn@users.noreply.github.com>
2024-04-29 19:55:27 +0400

    Created class for the post
    I have created class for post and included an import random code line

--- PartA.py
+++ PartA.py
@@ -1,1 +1,9 @@
+import random
 
+class Post:
+    def __init__(self, postID, views, creatorname, postdate, posttime):
+        self.postID = postID
+        self.views = views
+        self.creatorname = creatorname
+        self.postdate = postdate
+        self.posttime = posttime
